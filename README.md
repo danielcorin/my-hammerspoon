118c75501b25cc7252d98326517882ec94f798fa
Daniel Corin <dcorin6@gmail.com>
2015-07-17 11:29:47 -0700

    fix dock padding issue for resizing

---
+++ README.md
@@ -0,0 +1,2 @@
+My Hammerspoon config
+=====================
--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
 -- Beginning
 hs.window.animationDuration = 0
-local all = {"cmd", "alt", "ctrl"}
+local CTRL = "ctrl"
+local ALT = "alt"
+local CMD = "cmd"
+local all = {CTRL, ALT, CMD}
+local right_2 = {ALT, CMD}
+local left_2 = {CTRL, ALT}
+local split = {CTRL, CMD}
 
 -- Window resizing
 
@@ -8,7 +14,7 @@ hs.hotkey.bind(all, "Left", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
-  local max = screen:frame()
+  local max = screen:fullFrame()
 
   f.x = max.x
   f.y = max.y
@@ -21,7 +27,7 @@ hs.hotkey.bind(all, "Right", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
-  local max = screen:frame()
+  local max = screen:fullFrame()
 
   f.x = max.x + (max.w / 2)
   f.y = max.y
@@ -34,7 +40,7 @@ hs.hotkey.bind(all, "Up", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
-  local max = screen:frame()
+  local max = screen:fullFrame()
 
   f.x = max.x
   f.y = max.y
@@ -46,6 +52,24 @@ end)
 -- Multi monitor
 hs.hotkey.bind(all, 'N', hs.grid.pushWindowNextScreen)
 hs.hotkey.bind(all, 'P', hs.grid.pushWindowPrevScreen)
+
+-- local caffeine = hs.menubar.new()
+-- function setCaffeineDisplay(state)
+--     if state then
+--         caffeine:setTitle("AWAKE")
+--     else
+--         caffeine:setTitle("SLEEPY")
+--     end
+-- end
+
+-- function caffeineClicked()
+--     setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+-- end
+
+-- if caffeine then
+--     caffeine:setClickCallback(caffeineClicked)
+--     setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+-- end
 
 -- Caffeine
 hs.hotkey.bind(all, 'C', function ()
@@ -63,9 +87,23 @@ hs.hotkey.bind(all, 'S', function ()
 	caffeine_status()
 end)
 
--- Config reload
-hs.hotkey.bind(all, "R", function()
-  hs.reload()
+-- Show config
+hs.hotkey.bind(all, 'E', function ()
+  io.popen('open ~/.hammerspoon/init.lua')
 end)
+
+-- Config reload
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
 
